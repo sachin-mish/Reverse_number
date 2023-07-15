efe7f0255ff282e4d9b03c20af7dd089e56f30a5
Sachin mishra <76901807+sachin-mish@users.noreply.github.com>
2023-07-15 17:44:27 +0530

    Add files via upload

---
+++ reverse number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include<math.h>
+
+using namespace std;
+
+int main() {
+  
+ int n;
+ 
+  cin>>n;
+  
+ int rev_num=0;
+ 
+  while(n>0){
+    
+  int lastnumber=n%10;
+   
+   rev_num= rev_num*10+lastnumber;
+ 
+     n=n/10;
+  
+ }
+ 
+ cout<< rev_num;
+ 
+return 0;
+
+}
